e3816a905829f182c189d363d6bcf4a6ae526045
Dominic Cupo <Dominic.Cupo@gmail.com>
2018-03-24 18:11:11 -0400

    Added Binary designation

--- src/paste_arduino/src/wpi_rbe501_pneumatics/wpi_rbe501_pneumatics.ino
+++ src/paste_arduino/src/wpi_rbe501_pneumatics/wpi_rbe501_pneumatics.ino
@@ -1,4 +1,4 @@
-/*************************************************** 
+/***************************************************
   Worcester Polytechnic Institute
   RBE501 Robot Dynamics, Team 5, Spring 2018
   Daniel Sullivan, Dominic Cupo, Yan Wang
@@ -8,24 +8,24 @@
   - You must install the rosserial library in your Arduino IDE
   - You must install the Sparkfun AVR boards
       - see:https://github.com/sparkfun/Arduino_Boards
-  - The board this code is running in is the 
+  - The board this code is running in is the
       - Sparkfun Pro Micro (CHOOSE THE 5V OPTION under "Processor"
-  
+
   Designed to interface with four P20381RF pneumatic valves
 
-  This code accepts ROS boolean commands as well as 
+  This code accepts ROS boolean commands as well as
   four discrete inputs from the controller box. The
   ROS and discrete inputs are OR-ed to produce digital
-  outputs for four NPN transistor bases. The states of 
+  outputs for four NPN transistor bases. The states of
   these transistor commands are published to the ROS NODE.
 
-  This code expects a single byte message from ROS that 
+  This code expects a single byte message from ROS that
   encapsulates all valve commands in the least significant
   four bits as shown in the table below.
 
-  Similarly, the code will publish a single byte message of 
+  Similarly, the code will publish a single byte message of
   the discrete states as presented in the table below.
-  
+
   Binary       Decimal        V1   V2   V3   V4
   -----------------------------------------------
   1111         15             on   on   on   on
@@ -33,7 +33,7 @@
   1101         13             on   on   off  on
   1100         12             on   on   off  off
   1011         11             on   off  on   on
-  1010         10             on   off  on   off     
+  1010         10             on   off  on   off
   1001         9              on   off  off  on
   1000         8              on   off  off  off
   0111         7              off  on   on   on
@@ -44,13 +44,13 @@
   0010         2              off  off  on   off
   0001         1              off  off  off  on
   0000         0              off  off  off  off
-  
+
  ****************************************************/
- 
+
 // ROS Serial Setup:
-#include <ros.h>             
-#include <std_msgs/Bool.h>   
-#include <std_msgs/Byte.h>   
+#include <ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Byte.h>
 
 ros::NodeHandle nh;           // ROS Node handle
 
@@ -61,22 +61,22 @@ bool ROS_V3_Cmd = 0;      // Valve 3 ROS Command
 bool ROS_V4_Cmd = 0;      // Valve 4 ROS Command
 
 // ROS Subscriber Callback Function:
-void ROS_Cmd_Callback(const std_msgs::Byte& ROS_Cmd_msg){
-    // Perform bit masking to find each valve command
-    ROS_V1_Cmd = ROS_Cmd_msg.data & 1000;
-    ROS_V2_Cmd = ROS_Cmd_msg.data & 0100;
-    ROS_V3_Cmd = ROS_Cmd_msg.data & 0010;
-    ROS_V4_Cmd = ROS_Cmd_msg.data & 0001;
+void ROS_Cmd_Callback(const std_msgs::Byte& ROS_Cmd_msg) {
+  // Perform bit masking to find each valve command
+  ROS_V1_Cmd = ROS_Cmd_msg.data & B1000;
+  ROS_V2_Cmd = ROS_Cmd_msg.data & B0100;
+  ROS_V3_Cmd = ROS_Cmd_msg.data & B0010;
+  ROS_V4_Cmd = ROS_Cmd_msg.data & B0001;
 }
 
 // Instantiate ROS Subscribers
-ros::Subscriber<std_msgs::Byte> ROS_Valve_Sub("ROS Valve Commands", &ROS_Cmd_Callback);
+ros::Subscriber<std_msgs::Byte> ROS_Valve_Sub("ROS_Valve_Commands", &ROS_Cmd_Callback);
 
 // Initialize ROS Publishers
 std_msgs::Byte ROS_Status_msg;      // ROS Feedback Message
 
 // Instantiate ROS Publishers
-ros::Publisher ROS_Valve_Pub("ROS Valve Status", &ROS_Status_msg);  
+ros::Publisher ROS_Valve_Pub("ROS_Valve_Status", &ROS_Status_msg);
 
 // Button input pins:
 const int V1_Button_Pin = 3;   // Valve 1 Manual Button Read Pin
@@ -107,94 +107,94 @@ byte Output_msg = 0;
 
 void setup() {
 
-     // Serial monitor initialization for debugging
-     Serial.begin(9600);
+  // Serial monitor initialization for debugging
+  Serial.begin(9600);
 
-     // ROS initialization
-     nh.initNode();
+  // ROS initialization
+  nh.initNode();
 
-     // ROS subscribers
-     nh.subscribe(ROS_Valve_Sub);
+  // ROS subscribers
+  nh.subscribe(ROS_Valve_Sub);
 
-     // ROS advertisers
-     nh.advertise(ROS_Valve_Pub);
-     
-     // Button pin inputs:
-     pinMode(V1_Button_Pin, INPUT);
-     pinMode(V2_Button_Pin, INPUT);
-     pinMode(V3_Button_Pin, INPUT);
-     pinMode(V4_Button_Pin, INPUT);
+  // ROS advertisers
+  nh.advertise(ROS_Valve_Pub);
 
-     // Button pin inputs:
-     pinMode(V1_Out_Pin, OUTPUT);
-     pinMode(V2_Out_Pin, OUTPUT);
-     pinMode(V3_Out_Pin, OUTPUT);
-     pinMode(V4_Out_Pin, OUTPUT);
+  // Button pin inputs:
+  pinMode(V1_Button_Pin, INPUT);
+  pinMode(V2_Button_Pin, INPUT);
+  pinMode(V3_Button_Pin, INPUT);
+  pinMode(V4_Button_Pin, INPUT);
+
+  // Button pin inputs:
+  pinMode(V1_Out_Pin, OUTPUT);
+  pinMode(V2_Out_Pin, OUTPUT);
+  pinMode(V3_Out_Pin, OUTPUT);
+  pinMode(V4_Out_Pin, OUTPUT);
 }
 
 void loop() {
 
-     // Update button states
-     V1_Button_State = digitalRead(V1_Button_Pin);
-     V2_Button_State = digitalRead(V2_Button_Pin);
-     V3_Button_State = digitalRead(V3_Button_Pin);
-     V4_Button_State = digitalRead(V4_Button_Pin);
+  // Update button states
+  V1_Button_State = digitalRead(V1_Button_Pin);
+  V2_Button_State = digitalRead(V2_Button_Pin);
+  V3_Button_State = digitalRead(V3_Button_Pin);
+  V4_Button_State = digitalRead(V4_Button_Pin);
 
-     // Debugging: print button states to serial monitor
-     Serial.print("V1: ");
-     Serial.print(V1_Button_State);
-     Serial.print("     ");
-     Serial.print("V2: ");
-     Serial.print(V2_Button_State);
-     Serial.print("     ");
-     Serial.print("V3: ");
-     Serial.print(V3_Button_State);
-     Serial.print("     ");
-     Serial.print("V4: ");
-     Serial.println(V4_Button_State);
+  // Debugging: print button states to serial monitor
+  Serial.print("V1: ");
+  Serial.print(V1_Button_State);
+  Serial.print("     ");
+  Serial.print("V2: ");
+  Serial.print(V2_Button_State);
+  Serial.print("     ");
+  Serial.print("V3: ");
+  Serial.print(V3_Button_State);
+  Serial.print("     ");
+  Serial.print("V4: ");
+  Serial.println(V4_Button_State);
 
-     // Reset the output message:
-     Output_msg = 0;
-         
-     // Update Valve 1 Command
-     if (V1_Button_State == HIGH || ROS_V1_Cmd == HIGH) {
-          digitalWrite(V1_Out_Pin,HIGH);
-          Output_msg = Output_msg | 1000;
-     } else {
-          digitalWrite(V1_Out_Pin,LOW);
-     }
-     
-     // Update Valve 2 Command
-     if (V2_Button_State == HIGH || ROS_V2_Cmd == HIGH) {
-          digitalWrite(V2_Out_Pin,HIGH);
-          Output_msg = Output_msg | 0100;
-     } else {
-          digitalWrite(V2_Out_Pin,LOW);
-     }
+  // Reset the output message:
+  Output_msg = 0;
 
-     // Update Valve 3 Command
-     if (V3_Button_State == HIGH || ROS_V3_Cmd == HIGH) {
-          digitalWrite(V3_Out_Pin,HIGH);
-          Output_msg = Output_msg | 0010;
-     } else {
-          digitalWrite(V3_Out_Pin,LOW);
-     }
+  // Update Valve 1 Command
+  if (V1_Button_State == HIGH || ROS_V1_Cmd == HIGH) {
+    digitalWrite(V1_Out_Pin, HIGH);
+    Output_msg = Output_msg | B1000;
+  } else {
+    digitalWrite(V1_Out_Pin, LOW);
+  }
 
-     // Update Valve 4 Command
-     if (V4_Button_State == HIGH || ROS_V4_Cmd == HIGH) {
-          digitalWrite(V4_Out_Pin,HIGH);
-          Output_msg = Output_msg | 0001;
-     } else {
-          digitalWrite(V4_Out_Pin,LOW);
-     }
+  // Update Valve 2 Command
+  if (V2_Button_State == HIGH || ROS_V2_Cmd == HIGH) {
+    digitalWrite(V2_Out_Pin, HIGH);
+    Output_msg = Output_msg | B0100;
+  } else {
+    digitalWrite(V2_Out_Pin, LOW);
+  }
 
-     // Publish the status message
-     ROS_Valve_Pub.publish( &ROS_Status_msg );
+  // Update Valve 3 Command
+  if (V3_Button_State == HIGH || ROS_V3_Cmd == HIGH) {
+    digitalWrite(V3_Out_Pin, HIGH);
+    Output_msg = Output_msg | B0010;
+  } else {
+    digitalWrite(V3_Out_Pin, LOW);
+  }
 
-     // Update the ROS Node
-     nh.spinOnce();
+  // Update Valve 4 Command
+  if (V4_Button_State == HIGH || ROS_V4_Cmd == HIGH) {
+    digitalWrite(V4_Out_Pin, HIGH);
+    Output_msg = Output_msg | B0001;
+  } else {
+    digitalWrite(V4_Out_Pin, LOW);
+  }
 
-     // Delay for stability
-     delay(1);        
+  // Publish the status message
+  ROS_Valve_Pub.publish( &ROS_Status_msg );
+
+  // Update the ROS Node
+  nh.spinOnce();
+
+  // Delay for stability
+  delay(1);
 
 }
